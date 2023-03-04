969117da14716d825cf26958f89a6d3b96f63bd0
Dmitrii <lexur-02@yandex.ru>
2023-03-04 18:29:16 +0500

    fix

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:razdva122/PKO-tournament-simulator.git master:gh-pages
+git push -f git@github.com:razdva122/PKO-tournament-simulator.git main:gh-pages
 cd -
