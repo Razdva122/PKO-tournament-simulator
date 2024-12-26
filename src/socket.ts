37866b7aa7984f24be7c36973031be026cc04fb4
Dmitrii <lexur-02@yandex.ru>
2024-12-26 20:18:23 +0500

    update route

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import { io } from 'socket.io-client'
 
-export const socketURL = process.env.NODE_ENV === 'production' ? '158.160.37.112:3000' : 'localhost:3000'
+export const socketURL = process.env.NODE_ENV === 'production' ? 'pkotournamentsbackend.online:3000' : 'localhost:3000'
 
 export const socket = io(socketURL, {
   withCredentials: true
